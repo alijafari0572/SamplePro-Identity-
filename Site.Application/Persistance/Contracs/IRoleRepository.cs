7f86c19d0f3386eb992c6140c51de42f10bdd57d
alijafari0572 <alijafari0572@gmail.com>
2023-02-04 16:18:56 +0330

    15/11/01:16:18

---
+++ Site.Application/Persistance/Contracs/IRoleRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site.Application.Persistance.Contracs
+{
+    public class IRoleRepository : IGenericRepository<IdentityRole>
+    {
+        public Task<IdentityRole> Add(IdentityRole entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityRole> Delete(IdentityRole entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<IdentityRole>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityRole> GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityRole> Update(IdentityRole entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Site.Application/Persistance/Contracs/IUserRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site.Application.Persistance.Contracs
+{
+    public class IUserRepository : IGenericRepository<IdentityUser>
+    {
+        public Task<IdentityUser> Add(IdentityUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityUser> Delete(IdentityUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<IdentityUser>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityUser> GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityUser> Update(IdentityUser entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
